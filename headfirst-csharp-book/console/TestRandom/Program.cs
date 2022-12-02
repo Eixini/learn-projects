297c097ee2157b34d593b07d76457697d0170fd1
Eixini <eixi16@gmail.com>
2022-12-02 15:48:38 +0300

    Add TestRandom project

---
+++ headfirst-csharp-book/console/TestRandom/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TestRandom;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Test Random class." + '\n');
+
+        Random random = new Random();
+        int randomInt = random.Next();
+        Console.WriteLine($"Random int = {randomInt}" + '\n');
+
+        int zeroToNine = random.Next(10);
+        Console.WriteLine($"Random [0;9] = {zeroToNine}" + '\n');
+
+        int dieRoll = random.Next(1, 7);
+        Console.WriteLine($"Roll = {dieRoll}" + '\n');
+
+        double randomDouble = random.NextDouble();
+        Console.WriteLine($"Random double (x100) = {randomDouble * 100}");
+        Console.WriteLine($"Random double (to float) (x100) = {(float)randomDouble * 100F}");
+        Console.WriteLine($"Random double (to decimal) (x100) = {(decimal)randomDouble * 100M}" + '\n');
+
+        int zeroOrOne = random.Next(2);
+        bool coinFlip = Convert.ToBoolean(zeroOrOne);
+        Console.WriteLine($"Coin flip = {coinFlip}");
+    }
+}
