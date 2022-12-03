ad76f42fa286a506accba4884212dabd79f0ebc2
Eixini <eixi16@gmail.com>
2022-12-03 15:24:24 +0300

    Add SwordBattle project

---
+++ headfirst-csharp-book/console/SwordBattle/SwordDamage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SwordBattle;
+
+internal class SwordDamage
+{
+    public const int BASE_DAMAGE = 3;
+    public const int FLAME_DAMAGE = 2;
+
+    public SwordDamage(int startingRoll)
+    {
+        roll = startingRoll;
+        CalculateDamage();
+    }
+
+    private int roll;
+    private bool flaming;
+    private bool magic;
+
+    public int Roll
+    {
+        get { return roll; }
+        set { 
+            roll = value;
+            CalculateDamage();
+        }
+    }
+
+    /// <summary>
+    /// true - если меч огненный, false - в противном случае
+    /// </summary>
+    public bool Flaming
+    {
+        get { return flaming; }
+        set
+        {
+            flaming = value;
+            CalculateDamage();
+        }
+    }
+
+    /// <summary>
+    /// true - если меч волшебный, false - в противном случае
+    /// </summary>
+    public bool Magic
+    {
+        get { return magic; }
+        set { 
+            magic = value;
+            CalculateDamage();
+        }
+    }
+
+    /// <summary>
+    /// Содержит суммированный урон
+    /// </summary>
+    public int Damage { get; private set; }
+
+    public void CalculateDamage()
+    {
+        decimal magicMultiplier = 1M;
+        if (Magic) magicMultiplier = 1.75M;
+
+        Damage = BASE_DAMAGE;
+        Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+        if (Flaming) Damage += FLAME_DAMAGE;
+    }
+
+}
