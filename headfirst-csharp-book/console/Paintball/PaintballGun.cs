bca30363e0eb7dec4be99aad9a306f4e13a3053a
Eixini <eixi16@gmail.com>
2022-12-05 14:43:39 +0300

    Add Paintball project

---
+++ headfirst-csharp-book/console/Paintball/PaintballGun.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Paintball;
+
+internal class PaintballGun
+{
+
+    private int balls = 0;
+
+    public PaintballGun(int balls, int magazineSize, bool loaded)
+    {
+        this.balls = balls;
+        MagazineSize= magazineSize;
+        if (!loaded) Reload();
+    }
+
+    public int Balls
+    {
+        get { return balls; }
+        set {
+            if (value > 0)
+                balls = value;
+            Reload();
+        }
+    }
+
+    public int MagazineSize { get; private set; } = 16;
+
+    public int  BallsLoaded { get; private set; }
+
+    public static int ReadInt(int intValue, string message)
+    {
+        Console.WriteLine($"{message} [{intValue}]");
+        string line = Console.ReadLine();
+        if(int.TryParse(line, out int value))
+        {
+            Console.WriteLine($" using value {value}");
+            return value;
+        }
+        else
+        {
+            Console.WriteLine($" using default value {intValue}");
+            return intValue;
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        return BallsLoaded == 0;
+    }
+
+    public void Reload()
+    {
+        if (balls > MagazineSize)
+            BallsLoaded = MagazineSize;
+        else
+            BallsLoaded = balls;
+    }
+
+    public bool Shoot()
+    {
+        if (BallsLoaded == 0)
+            return false;
+        BallsLoaded--;
+        balls--;
+        return true;
+    }
+}
---
+++ headfirst-csharp-book/console/Paintball/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Paintball;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcom to Paintball!" + '\n');
+
+        int numberOfBalls = PaintballGun.ReadInt(20, "Number of balls");
+        int magazineSize = PaintballGun.ReadInt(16, "Magazine size");
+
+        Console.WriteLine($"Loaded [false]: ");
+        bool.TryParse(Console.ReadLine(),out bool isLoaded);
+
+        PaintballGun gun = new PaintballGun(numberOfBalls, magazineSize, isLoaded);
+
+        while (true)
+        {
+            Console.WriteLine($"{gun.Balls} balls, {gun.BallsLoaded} loaded.");
+            if (gun.IsEmpty())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;             // Немножко отсебятины
+                Console.WriteLine("WARNING: You're out of ammo!");
+                Console.ResetColor();
+            }
+            Console.WriteLine("Space to shoot, r to reloaded, + tp add ammo, q to quit.");
+            char key = Console.ReadKey(true).KeyChar;
+            if (key == ' ')
+                Console.WriteLine($"Shooting returned {gun.Shoot()}");
+            else if (key == 'r')
+                gun.Reload();
+            else if (key == '+')
+                gun.Balls += gun.MagazineSize;
+            else if (key == 'q')
+                return;
+        }
+    }
+}
