bb8ecce62fbd2af49b6e171e75566c8222088958
Eixini <eixi16@gmail.com>
2022-12-07 14:47:09 +0300

    Add CardWeaponBattle project

---
+++ headfirst-csharp-book/console/CardWeaponBattle/WeaponDamage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CardWeaponBattle;
+
+internal abstract class WeaponDamage
+{
+    public const int BASE_DAMAGE = 3;
+    public const int FLAME_DAMAGE = 2;
+
+    public WeaponDamage(int startingRoll)
+    {
+        roll = startingRoll;
+        CalculateDamage();
+    }
+
+    private int roll;
+    private bool flaming;
+    private bool magic;
+
+    public int Roll
+    {
+        get { return roll; }
+        set
+        {
+            roll = value;
+            CalculateDamage();
+        }
+    }
+
+    /// <summary>
+    /// true - если оружие огненное, false - в противном случае
+    /// </summary>
+    public bool Flaming
+    {
+        get { return flaming; }
+        set
+        {
+            flaming = value;
+            CalculateDamage();
+        }
+    }
+
+    /// <summary>
+    /// true - если оружие огненное, false - в противном случае
+    /// </summary>
+    public bool Magic
+    {
+        get { return magic; }
+        set
+        {
+            magic = value;
+            CalculateDamage();
+        }
+    }
+
+    /// <summary>
+    /// Содержит суммированный урон
+    /// </summary>
+    public int Damage { get; protected set; }
+
+    public abstract void CalculateDamage();
+
+}
