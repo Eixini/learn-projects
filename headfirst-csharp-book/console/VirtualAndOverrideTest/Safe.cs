14c05c71e3d7027a68dd361b6f95f6900882b94b
Eixini <eixi16@gmail.com>
2022-12-09 16:29:58 +0300

    Add VirtualAndOverrideTest project

---
+++ headfirst-csharp-book/console/VirtualAndOverrideTest/Safe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VirtualAndOverrideTest;
+
+internal class Safe
+{
+    private string constents = "precious jewels";
+    private string safeCombination = "12345";
+
+    public string Open(string combination)
+    {
+        if (combination == safeCombination)
+            return constents;
+        return "";
+    }
+
+    public void PickLock(Locksmith lockpicker)
+    {
+        lockpicker.Combination = safeCombination;
+    }
+}
---
+++ headfirst-csharp-book/console/VirtualAndOverrideTest/SafeOwner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VirtualAndOverrideTest;
+
+internal class SafeOwner
+{
+    private string valuables = "";
+    public void ReceiveContents(string safeContents)
+    {
+        valuables = safeContents;
+        Console.WriteLine($"Thank you for returning my {valuables}!");
+    }
+}
