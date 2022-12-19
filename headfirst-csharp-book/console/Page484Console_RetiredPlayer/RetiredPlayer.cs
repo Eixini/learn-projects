28b8654102d2b7c58205fbabffccedcbe098a0ad
Eixini <eixi16@gmail.com>
2022-12-19 13:46:14 +0300

    Add Page484Console_RetiredPlayer project

---
+++ headfirst-csharp-book/console/Page484Console_RetiredPlayer/RetiredPlayer.cs
@@ -0,0 +1,14 @@
+﻿namespace Page484Console_RetiredPlayer;
+
+internal class RetiredPlayer
+{
+    public string Name { get; private set; }
+    public int YearRetired { get; private set; }
+
+    public RetiredPlayer(string player, int yearRetired)
+    {
+        Name = player;
+        YearRetired = yearRetired;
+    }
+
+}
