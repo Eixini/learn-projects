cd21c4ba779429f25c155de49d95289238cad8ec
Eixini <eixi16@gmail.com>
2022-12-20 19:24:28 +0300

    Add JimmyComic project

---
+++ headfirst-csharp-book/console/JimmyComic/JimmyComic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JimmyComic;
+
+public class Program  {
+    public static void Main(string[] args)
+    {
+        var mostExpensive =
+            from comic in Comic.Catalog
+            where Comic.Prices[comic.Issue] > 500
+            orderby Comic.Prices[comic.Issue] descending
+            select $"{comic} is worth {Comic.Prices[comic.Issue]} $";
+
+        foreach (var comic in mostExpensive)
+            Console.WriteLine(comic);
+    }
+}
