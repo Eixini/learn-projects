954aaf3e58dc888daadf83e31c80d2d532da23a0
Eixini <eixi16@gmail.com>
2022-12-24 15:18:39 +0300

    Add Ch10_StreamWriterTest project

---
+++ headfirst-csharp-book/console/Ch10_StreamWriterTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Ch10_StreamWriterTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        StreamWriter streamWriter = new StreamWriter("secret_plan.txt");
+
+        streamWriter.WriteLine("How I'll defeat Captain Amazing");
+        streamWriter.WriteLine("Another genius secret plan by The Swindler");
+        streamWriter.WriteLine("I'll unleash my army of clones upon the citizens of Objectville.");
+
+        string location = "the mall";
+        for(int number = 1; number <= 5; number++)
+        {
+            streamWriter.WriteLine($"Clone #{0} attacks {1}", number, location);
+            location = (location == "the mall") ? "downtown" : "the mall";
+        }
+        streamWriter.Close();
+    }
+}
