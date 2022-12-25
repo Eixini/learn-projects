f2dfbdce38627ef609b2f451e02942f6a4a724ac
Eixini <eixi16@gmail.com>
2022-12-25 18:32:11 +0300

    Add FinalizationTest project

---
+++ headfirst-csharp-book/console/FinalizationTest/EvilClone.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalizationTest;
+
+internal class EvilClone
+{
+    public static int CloneCount = 0;
+    public int CloneID { get; } = ++CloneCount;
+
+    public EvilClone() => Console.WriteLine($"Clone {CloneID} is wreaking havoc");
+    ~EvilClone() => Console.WriteLine($"Clone #{CloneID} destroyed");
+}
---
+++ headfirst-csharp-book/console/FinalizationTest/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FinalizationTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+        var clones = new List<EvilClone>();
+        while (true)
+        {
+            switch (Console.ReadKey(true).KeyChar)
+            {
+                case 'a':
+                    clones.Add(new EvilClone());
+                    break;
+                case 'c':
+                    Console.WriteLine($"Clearing list at time {stopwatch.ElapsedMilliseconds}");
+                    break;
+                case 'g':
+                    Console.WriteLine($"Collecting at time {stopwatch.ElapsedMilliseconds}");
+                    GC.Collect();
+                    break;
+            };
+        }
+    }
+}
