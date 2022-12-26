705315094bc6b820db5f141d73c2d7ac11b82b9c
Eixini <eixi16@gmail.com>
2022-12-26 16:34:50 +0300

    Add OutAndRefTest project

---
+++ headfirst-csharp-book/console/OutAndRefTest/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OutTest;
+
+internal class Program
+{
+    public static int ReturnThreeValues(int value, out double half, out int twice)
+    {
+        half = value / 2f;
+        twice = value * 2;
+        return value + 1;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+        if(int.TryParse(Console.ReadLine(), out int input))
+        {
+            var output1 = ReturnThreeValues(input, out double output2, out int output3);
+            Console.WriteLine($@"Outputs:
+                              plus one = {output1}
+                              half     = {output2}
+                              twice    = {output3}");
+        }
+    }
+}
