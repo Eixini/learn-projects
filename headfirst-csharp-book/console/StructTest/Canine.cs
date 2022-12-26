d146fdbca544a69f707cdae58187d09040df415b
Eixini <eixi16@gmail.com>
2022-12-26 14:18:30 +0300

    Add StructTest project

---
+++ headfirst-csharp-book/console/StructTest/Canine.cs
@@ -0,0 +1,15 @@
+﻿namespace StructTest;
+
+internal class Canine
+{
+    public string Name { get; set; }
+    public string Breed { get; set; }
+
+    public Canine(string name, string breed)
+    {
+        this.Name = name;
+        this.Breed = breed;
+    }
+
+    public void Speak() => Console.WriteLine($"My name is {this.Name} and I'm a {this.Breed}");
+}
---
+++ headfirst-csharp-book/console/StructTest/Dog.cs
@@ -0,0 +1,15 @@
+﻿namespace StructTest;
+
+internal struct Dog
+{
+    public string Name { get; set; }
+    public string Breed { get; set; }
+
+    public Dog(string name, string breed)
+    {
+        this.Name = name;
+        this.Breed = breed;
+    }
+
+    public void Speak() => Console.WriteLine($"My name is {this.Name} and I'm a {this.Breed}");
+}
