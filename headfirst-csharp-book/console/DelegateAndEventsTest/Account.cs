ebae6960d1817dcac8a8d4c96c4f7aba105ac849
Eixini <eixi16@gmail.com>
2022-12-30 18:31:33 +0300

    Add DelegateAndEventsTest project

---
+++ headfirst-csharp-book/console/DelegateAndEventsTest/Account.cs
@@ -0,0 +1,25 @@
+﻿namespace DelegateAndEventsTest;
+
+public delegate void AccountHandler(string message);
+internal class Account
+{
+	public int sum;
+	AccountHandler? taken;
+
+	public Account(int sum) => this.sum = sum;
+
+	public void RegisterHandler(AccountHandler del) => taken += del;
+	public void UnregisterHandler(AccountHandler del) => taken -= del;
+
+	public void Add(int sum) => this.sum += sum;
+	public void Take(int sum)
+	{
+		if (this.sum >= sum)
+		{
+			this.sum -= sum;
+			taken?.Invoke($"Со счета списано {sum} y.e");
+		}
+		else
+			taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} y.e");
+	}
+}
---
+++ headfirst-csharp-book/console/DelegateAndEventsTest/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DelegateAndEventsTest;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Account account = new Account(200);
+
+		account.RegisterHandler(PrintSimpleMessage);
+		account.RegisterHandler(PrintColorMessage);
+
+		account.Take(100);
+		account.Take(100);
+
+		account.UnregisterHandler(PrintColorMessage);
+
+		account.Take(50);
+	}
+
+	void  PrintSimpleMessage(string message) => Console.WriteLine(message);
+	void PrintColorMessage(string message)
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine(message);
+		Console.ResetColor();
+	}
+}
