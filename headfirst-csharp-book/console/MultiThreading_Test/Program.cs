f354bc758d8da892a5be9c8fb5dde743699edae3
Eixini <eixi16@gmail.com>
2023-01-01 17:08:53 +0300

    Add MultiThreading_Test project

---
+++ headfirst-csharp-book/console/MultiThreading_Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+
+namespace MultiThreading_Test;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+
+		Thread myThread_1 = new Thread(() => {
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine($"Второй поток: {i}");
+				Thread.Sleep(500);
+			}
+		});
+		myThread_1.Start();
+
+		Thread myThread_2 = new Thread(() => {
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine($"Третий поток: {i}");
+				Thread.Sleep(1500);
+			}
+		});
+		myThread_2.Start();
+
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine($"Главный поток: {i}");
+			Thread.Sleep(2000);
+		}
+	}
+
+}
