f70d3254fc1571943c5f195d9f75f26ab13e6f43
Eixini <eixi16@gmail.com>
2023-01-02 16:23:19 +0300

    Add AsyncTest project

---
+++ headfirst-csharp-book/console/AsyncTest/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AsyncTest;
+
+internal class Program
+{
+	async static Task Main(string[] args)
+	{
+		var tomTask = PrintNameAsync("Tom");
+		var bobTask =  PrintNameAsync("Bob");
+		var samTask =  PrintNameAsync("Sam");
+
+		await tomTask;
+		await bobTask;
+		await samTask;
+
+		async Task PrintNameAsync(string name)
+		{
+			await Task.Delay(3000);
+			Console.WriteLine(name);
+		}
+	}
+}
