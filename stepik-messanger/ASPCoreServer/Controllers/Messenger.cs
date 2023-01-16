c9619f6cb8ffecf485338de494413722c3b6250a
Eixini <eixi16@gmail.com>
2023-01-16 20:03:16 +0300

    Add ASPCoreServer project

---
+++ stepik-messanger/ASPCoreServer/Controllers/Messenger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mymessenger_stepik;
+using Newtonsoft.Json;
+
+namespace ASPCoreServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Messenger : ControllerBase
+    {
+        static List<Message> ListOfMessages = new List<Message>();
+        // GET api/<Messenger>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string OutputString = "Not found";
+            if ((id < ListOfMessages.Count) && (id >= 0))
+            {
+                OutputString = JsonConvert.SerializeObject(ListOfMessages[id]);
+            }
+            //Console.WriteLine(String.Format($"Запрошено сообщение # {id}: {OutputString}"));
+            return OutputString;
+        }
+
+        // POST api/<Messenger>
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] Message msg)
+        {
+            if (msg == null)
+            {
+                return BadRequest();
+            }
+            ListOfMessages.Add(msg);
+            Console.WriteLine(String.Format($"Всего сообщений: {ListOfMessages.Count} , Посланное сообщение: {msg}"));
+            return new OkResult();
+        }
+
+    }
+}
--- stepik-messanger/mymessenger-stepik/Program.cs
+++ stepik-messanger/mymessenger-stepik/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace mymessenger_stepik;
+﻿using Newtonsoft.Json;
+
+namespace mymessenger_stepik;
 
 internal class Program
 {
+    private static int MessageID;
+    private static string UserName;
+    private static MessengerClientAPI API = new MessengerClientAPI();
+
+    private static void GetNewMessages()
+    {
+        Message message = API.GetMessage(MessageID);
+        while(message != null)
+        {
+            Console.WriteLine(message);
+            MessageID++;
+            message = API.GetMessage(MessageID);
+        }
+    }
+
     static void Main(string[] args)
     {
-        Message msg = new Message();
-        Console.WriteLine(msg.ToString());
+        MessageID = 1;
+        Console.WriteLine("Введите ваше имя: ");
+        UserName = Console.ReadLine();
+        string MessageText = "";
+
+        Thread myThread = new Thread(new ThreadStart(GetNewMessages));
+        myThread.Start();
+
+        while(MessageText != "exit")
+        {
+            GetNewMessages();
+            Console.WriteLine("Введите текст сообщения:");
+            MessageText = Console.ReadLine();
+            if(MessageText.Length > 1)
+            {
+                Message sendMessage = new Message(UserName, MessageText, DateTime.Now);
+                API.SendMessage(sendMessage);
+            }
+        }
     }
 }
