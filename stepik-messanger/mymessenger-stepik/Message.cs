76075ba92ffad683c6cec63e72481fa757ec7a9c
Eixini <eixi16@gmail.com>
2023-01-16 16:27:01 +0300

    Add mymessenger-stepik project

---
+++ stepik-messanger/mymessenger-stepik/Message.cs
@@ -0,0 +1,23 @@
+﻿namespace mymessenger_stepik;
+
+internal class Message
+{
+    public Message (string userName, string messageText, DateTime timeStamp)
+    {
+        UserName = userName;
+        MessageText = messageText;
+        TimeStamp = timeStamp;
+    }
+    public Message ()
+    {
+        UserName = "System";
+        MessageText = "Server is running...";
+        TimeStamp = DateTime.Now;
+    }
+
+    public string UserName { get; set; }
+    public string MessageText { get; set; }
+    public DateTime TimeStamp { get; set; }
+
+    public override string ToString() => String.Format($"{UserName} <{TimeStamp}>: {MessageText}");
+}
---
+++ stepik-messanger/mymessenger-stepik/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mymessenger_stepik;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Message msg = new Message();
+        Console.WriteLine(msg.ToString());
+    }
+}
