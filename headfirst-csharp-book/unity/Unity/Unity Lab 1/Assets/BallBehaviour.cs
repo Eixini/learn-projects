9070e0012ba38a3eb69302f8c239ab7135cedf94
Eixini <eixi16@gmail.com>
2023-01-16 18:49:26 +0300

    Add Unity Lab 1 project

---
+++ headfirst-csharp-book/unity/Unity/Unity Lab 1/Assets/BallBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 0;
+    public float DegressPerSecond = 180;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero,axis, DegressPerSecond * Time.deltaTime);
+        Debug.DrawRay(Vector3.zero, axis, Color.yellow, .5f);
+    }
+}
