9caf6ea77e01bd85a013fd8dd1529f990ba9784d
Eixini <eixi16@gmail.com>
2023-01-22 18:24:57 +0300

    Add query for insert one row into book table

---
+++ stepik-sql/book-store/insert-into-book-table.sql
@@ -0,0 +1,5 @@
+/* Вставка данных в таблицу book */
+INSERT INTO book (title,author,price,amount)
+       VALUES ('Мастер и Маргарита','Булгаков М.А.','670.99','3');
+/* Возврат таблицы */
+SELECT * FROM book;
