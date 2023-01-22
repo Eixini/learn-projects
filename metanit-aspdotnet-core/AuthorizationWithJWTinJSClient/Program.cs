bf10d36f89dd7200fc2a726468281e176bcaaced
Eixini <eixi16@gmail.com>
2023-01-22 20:46:51 +0300

    Add AuthorizationWithJWTinJSClient project

---
+++ metanit-aspdotnet-core/AuthorizationWithJWTinJSClient/Program.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthorizationWithJWTinJSClient;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        // Услованая БД
+        var people = new List<Person>
+        {
+            new Person("tom@gmail.com", "12345"),
+            new Person("bob@gmail.com", "55555")
+        };
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidIssuer = AuthOptions.ISSUER,
+                       ValidateAudience = true,
+                       ValidAudience = AuthOptions.AUDIENCE,
+                       ValidateLifetime = true,
+                       IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                       ValidateIssuerSigningKey = true
+                   };
+               });   
+
+        var app = builder.Build();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapPost("/login", (Person loginData) =>
+        {
+            // Поиск пользователя
+            Person? person = people.FirstOrDefault(p => p.Email == loginData.Email 
+                                                     && p.Password == loginData.Password);
+            // Если пользователь не найден, отправляется статусный код 401
+            if (person is null)
+                return Results.Unauthorized();
+
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Email) };
+            // Создание JWT-токенов
+            var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                claims: claims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(),SecurityAlgorithms.HmacSha256)
+                );
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            // Формирование ответа
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = person.Email
+            };
+
+            return Results.Json(response);
+        });
+        app.Map("/data", [Authorize] () => new { message = "Hello!" });
+
+        app.Run();
+    }
+
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer";
+        public const string AUDIENCE = "MyAuthClient";
+        const string KEY = "mysupersecret_secretkey!123";
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+    }
+
+    record class Person(string Email, string Password);
+}
---
+++ metanit-aspdotnet-core/AuthorizationWithJWTinJSClient/wwwroot/script.js
@@ -0,0 +1,59 @@
+﻿var tokenKey = "accessToken";
+
+// При нажатии на кнопку отправки формы идет запрос к /login для получения токена
+document.getElementById("submitLogin").addEventListener("click", async e => {
+    e.preventDefault();
+    // Отправление запроса и получение ответа
+    const response = await fetch("/login", {
+        method: "POST",
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+            })
+    });
+    // Если запрос прошел успешно
+    if (response.ok === true) {
+        // Получение данных
+        const data = await response.json();
+        // Изменение содержимого и видимость блоков на странице
+        document.getElementById("userName").innerText = data.username;
+        document.getElementById("userInfo").style.display = "block";
+        document.getElementById("userForm").style.display = "none";
+        // Сохранение в хранилище sessionStorage токен доступа
+        sessionStorage.setItem(tokenKey, data.access_token);
+    }
+    else  // Получение код статуса, если произошла ошибка
+        console.log("Status: ", response.status);
+});
+
+// Кнопка для обращения по пути "/data" для получения данных
+document.getElementById("getData").addEventListener("click", async e => {
+    e.preventDefault();
+    // Получение токена из sessionStorage
+    const token = sessionStorage.getItem(tokenKey);
+    // Отправка запроса к "/data"
+    const response = await fetch("/data", {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Authorization": "Bearer" + token  // Передача токена в заголовке
+        }
+    });
+
+    if (response.ok === true) {
+        const data = await response.json();
+        alert(data.message);
+    }
+    else
+        console.log("Status: ", response.status);
+});
+
+// Условный выход - удаление токена и изменение видимости блоков
+document.getElementById("logOut").addEventListener("click", e => {
+    e.preventDefault();
+    document.getElementById("userName").innerText = "";
+    document.getElementById("userInfo").style.display = "none";
+    document.getElementById("loginForm").style.display = "block";
+    sessionStorage.removeItem(tokenKey);
+});
