09985dd5f0541417d844fc102ed7cd0e6a965962
Eixini <eixi16@gmail.com>
2023-01-23 18:34:28 +0300

    Add MangoRestaurant solution

---
+++ microservices-samples/MangoRestaurant/Mango.Services.ProductAPI/Program.cs
@@ -0,0 +1,31 @@
+using Mango.Services.ProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
