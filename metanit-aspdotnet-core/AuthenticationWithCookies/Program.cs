b64d6fdf0225a07d26e919eefbc278a6fc539c0a
Eixini <eixi16@gmail.com>
2023-01-23 21:01:48 +0300

    Add AuthenticationWithCookies project

---
+++ metanit-aspdotnet-core/AuthenticationWithCookies/Program.cs
@@ -0,0 +1,97 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace AuthenticationWithCookies;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Условная БД с пользователями
+        var people = new List<Person>
+        {
+            new Person("tom@gmail.com","12345"),
+            new Person("bob@gmail.com","55555")
+        };
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Аутентификация с помощью куки
+        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+               .AddCookie(options => options.LoginPath = "/login");
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        app.UseAuthentication();    // Добавление middleware аутентификации
+        app.UseAuthorization();     // Добавление middleware авторизации
+
+        app.MapGet("/login", async (HttpContext context) =>
+        {
+            context.Response.ContentType = "text/html; charset=utf-8";
+            // HTML-форма для ввода логина и пароля
+            string loginForm = @"<!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset='utf-8' />
+                    <title>METANIT.COM</title>
+                </head>
+                <body>
+                    <h2>Login Form</h2>
+                    <form method='post'>
+                        <p>
+                            <label>Email</label><br />
+                            <input name='email' />
+                        </p>
+                        <p>
+                            <label>Password</label><br />
+                            <input type='password' name='password' />
+                        </p>
+                        <input type='submit' value='Login' />
+                    </form>
+                </body>
+            </html>";
+            await context.Response.WriteAsync(loginForm);
+        });
+
+        app.MapPost("/login", async (string? returnUrl, HttpContext context) =>
+        {
+            // Получение e-mail и пароль из формы
+            var form = context.Request.Form;
+            // Если e-mail и пароль не установлены, посылается код ошибки 400
+            if (!form.ContainsKey("email") || !form.ContainsKey("password"))
+                return Results.BadRequest("E-mail и/или пароль не установлены");
+
+            string email = form["email"];
+            string password = form["password"];
+
+            // Поиск пользователя
+            Person? person = people.FirstOrDefault(p => p.Email == email && p.Password == password);
+            // Если пользователь не найден, отправляется статусный код 401
+            if (person is null)
+                return Results.Unauthorized();
+
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Email) };
+            // Создается объект ClaimsIdentity
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+            // Установка аутентификационных куки
+            await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+            return Results.Redirect(returnUrl??"/");
+        });
+
+        app.MapGet("/logout", async (HttpContext context) =>
+        {
+            await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Results.Redirect("/login");
+        });
+
+        app.MapGet("/", [Authorize]() => $"Hello World!");
+
+        app.Run();
+    }
+
+    record class Person(string Email, string Password);
+}
