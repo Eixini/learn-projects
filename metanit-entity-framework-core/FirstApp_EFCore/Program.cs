720640ff18f3cef3b6fd2af6c1b0772880b4377b
Eixini <eixi16@gmail.com>
2023-01-25 18:54:43 +0300

    Add FirstApp_EFCore project

---
+++ metanit-entity-framework-core/FirstApp_EFCore/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FirstApp_EFCore;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            // Создание двух объектов User
+            User tom = new User { Name = "Tom", Age = 33 };
+            User alice = new User { Name = "Alice", Age = 26 };
+
+            // Добавление объектов в БД
+            db.Users.Add(tom);
+            db.Users.Add(alice);
+            db.SaveChanges();
+            Console.WriteLine("Объекты успешно сохранены!");
+
+            // Получение объектов из БД и вывод на консоль
+            var users = db.Users.ToList();
+            Console.WriteLine("Список объектов:");
+            foreach(User u in users)
+                Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+        }
+    }
+}
---
+++ metanit-entity-framework-core/FirstApp_EFCore/User.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstApp_EFCore;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
